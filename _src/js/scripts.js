ca13bd46a5879cf964a83600fd895d54b5985590
NENO\kadoo <kadoox@gmail.com>
2020-02-28 23:53:16 +0100

    data-code-show

--- _src/js/scripts.js
+++ _src/js/scripts.js
@@ -1,1 +1,22 @@
-console.log("ok")
+$(document).ready(function(){
+
+    console.log("ok");
+    
+    $(document).on("click","[data-code-show]", function(e){
+
+        var content = e.target.innerHTML;
+        var t = $(this);
+        var tc = t.children("[data-code]");
+        console.log(t);
+
+        if (tc.length > 0){
+            tc.slideUp(500, function(){tc.remove()})
+        } else{
+            var c = $('<div data-code><textarea>'+ content +'</textarea></div>').hide().fadeIn(500);
+            t.prepend(c);
+            content = "";
+        };
+
+    });
+
+})
